a434f124a674ddf5edd56c5ab238fa97c795031b
HarmonyMorse <harmonymorse@icloud.com>
2023-04-14 19:30:45 -0700

    In PhotoDetailViewController - filled in savePhoto; created method setTheme() and updateViews(), both called in viewDidLoad()

--- Photo Collection/Photo Collection/PhotoDetailViewController.swift
+++ Photo Collection/Photo Collection/PhotoDetailViewController.swift
@@ -18,14 +18,40 @@ class PhotoDetailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setTheme()
+        updateViews()
     }
     
     @IBAction func savePhoto(_ sender: UIBarButtonItem) {
+        guard let data = photoImageView.image?.pngData() else { fatalError("error 1 in savePhoto in PhotoDetailViewController") }
+        guard let titleText = titleTextField.text else { fatalError("error 2 in savePhoto in PhotoDetailViewController") }
+        
+        if let photo = photo {
+            photoController?.update(photo: photo, newData: data, newTitle: titleText)
+        } else {
+            photoController?.create(photo: Photo(imageData: data, title: titleText))
+        }
+        dismiss(animated: true, completion: nil)
     }
     
     @IBAction func addPhoto(_ sender: UIButton) {
+        
+    }
+    
+    func setTheme() {
+        guard let theme = themeHelper?.themePreference else { return }
+        if theme == "Dark" {
+            view.backgroundColor = .darkGray
+        } else {
+            view.backgroundColor = .blue
+        }
+    }
+    
+    func updateViews() {
+        guard let photo = photo else { fatalError("something wrong here bro, updateViews() in PhotoDetailViewController") }
+        
+        photoImageView.image = UIImage(data: photo.imageData)
+        titleTextField.text = photo.title
     }
     
 
